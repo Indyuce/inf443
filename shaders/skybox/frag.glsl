ac884d194ddcbf488c3a93569ba6b6604cbdde9d
Jules <cym.peyrat@yahoo.fr>
2023-05-30 13:34:54 +0200

    Horizon fog

--- shaders/skybox/frag.glsl
+++ shaders/skybox/frag.glsl
@@ -23,10 +23,6 @@ void main()
 {
     // Texture color
     vec3 current_color = vec3(texture(image_skybox, fragment.position));
-    
-    // Is the player looking at a water pixels
-    // TODO
-    vec2 projected = camera_direction.xy * abs(camera_position.z / camera_direction.z);
 
     // Underwater
     bool under_water = camera_position.z < 5f;
@@ -39,12 +35,18 @@ void main()
     /***********************************************************/
     else {
     
-        // Direct sunlight
+        // Skybox disappear in fog far away
+        vec3 horizon_fog_color  = vec3(1.0f, 1.0f, 1.0f);
         vec3 fragment_direction = normalize(fragment.position);
+        float fog_coefficient   = pow(1 - max(0, fragment_direction.z), 30);
+    
+        // Direct sunlight
         float direct_magnitude = direct * pow(max(dot(fragment_direction, -light_direction), 0), direct_exp);
         current_color += direct_magnitude * light_color;
-    }
 
+        // Horizon fog
+        current_color = mix(current_color, horizon_fog_color, fog_coefficient);
+    }
 
 	FragColor = vec4(current_color, 1.0);
 }
